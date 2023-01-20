e489b1b10d1a28754c515b746bf78cac75e3b415
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-01-20 18:08:38 +0100

    me gustaria consultar una cosa

--- src/app/components/fifth/fifth.component.ts
+++ src/app/components/fifth/fifth.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { FormAsyncValidatorsService } from 'src/app/services/form-async-validators.service';
 import { AsyncValidatorFn } from '@angular/forms';
+import { Observable ,delay, tap ,map , of } from 'rxjs';
+import { ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-fifth',
@@ -10,21 +12,27 @@ import { AsyncValidatorFn } from '@angular/forms';
 })
 export class FifthComponent {
 
-  validatorMiddleware:AsyncValidatorFn = (control:AbstractControl) => new Promise((rs,rj) => {
-    console.log('pokeMiddlewareValidator',control)
-    rs(null)
-  })
+  validatorMiddleware = ({value}:any):Observable<ValidationErrors | null> => {
+    //Haz por usar el of, ya que el Observable esta deprecated para una mayoria de usos.
+    return of<any>(value).pipe(
+      delay(1000),
+      tap(() => { console.log('entrando en el validator',value) }),
+      map(resp => null)
+    )
+  }
+
+  validatorPlaceholder = [Validators.required,Validators.minLength(6)];
 
   public register:UntypedFormGroup = new UntypedFormGroup({
-    name:new UntypedFormControl(undefined,[Validators.required,Validators.minLength(6)],undefined),
-    sidekick:new UntypedFormControl(undefined,[Validators.required,Validators.minLength(6)],undefined)
+    name:new UntypedFormControl(undefined,this.validatorPlaceholder,undefined),
+    sidekick:new UntypedFormControl(undefined,this.validatorPlaceholder,undefined)
   },{
-    updateOn:'blur',
-    validators:[],
+    //updateOn:'blur',
+    validators:[Validators.required],
     asyncValidators:[this.validatorMiddleware]
   });
 
-  registerSubmit(){console.log('valid form?',this.register.valid)}
+  registerSubmit(){}
 
   constructor(
     private form:FormBuilder,
