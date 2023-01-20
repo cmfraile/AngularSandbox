b08fca6ea767e67e83894522786661edc9cf5646
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-01-20 15:55:38 +0100

    vamos a hacer la prueba

--- src/app/components/fourth/fourth.component.ts
+++ src/app/components/fourth/fourth.component.ts
@@ -6,6 +6,9 @@ import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { delay, map, of, tap } from 'rxjs';
 import { Observable } from 'rxjs';
 import * as _ from 'underscore';
+import { HttpErrorResponse } from '@angular/common/http';
+
+interface digimon {name:string,img:string,level:string}
 
 @Component({
   selector: 'app-fourth',
@@ -18,18 +21,23 @@ export class FourthComponent {
 
     if(control.pristine){ return of(null) };
 
-    const check = () => (!this.formAsyncValidators.digiCheckName.includes(control.value)) ? { ['invalidTerm']:true } : null ;
-    
-    return of(null).pipe(
+    return this.formAsyncValidators.digimonQuery().pipe(
       delay(2000),
-      map(check)
+      tap(() => { console.log('query!') }),
+      map((resp:any) => {
+        let validation:ValidationErrors|null = null;
+        if(resp.error){validation = {['errorInQuery']:true}};
+        const digiArray:digimon[] = resp.map((x:digimon) => x.name);
+        if(!digiArray.includes(control.value)){validation = {['invalidTerm']:true}};
+        return validation
+      })
     )
-  
+
   }
 
   public digimonChoose:UntypedFormGroup = new UntypedFormGroup({
     digimon : new UntypedFormControl(undefined,undefined,[this.$asyncValidatorExample]),
-  })
+  },{updateOn:'blur'})
 
   login(){
     if(this.digimonChoose.invalid){return};
--- src/app/services/form-async-validators.service.ts
+++ src/app/services/form-async-validators.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { shuffle } from 'underscore';
@@ -10,17 +10,19 @@ interface digimon {name:string,img:string,level:string}
 })
 export class FormAsyncValidatorsService {
 
-  digiquery:string = 'https://digimon-api.vercel.app/api/digimon';
+  public digiquery:string = 'https://digimon-api.vercel.app/api/digimoan';
   digiCheckName:string[] = [] ;
   digiModals:{name:string,img:string}[] = [];
 
-  public digimonQuery():Observable<digimon[]>{return this.http.get<digimon[]>(this.digiquery)}
+  public digimonQuery():Observable<digimon[]>{return this.http.get<digimon[]>(this.digiquery)};
 
   constructor(private http:HttpClient){
-    this.digimonQuery().subscribe(resp => {
+    this.digimonQuery().subscribe({
+      next:(resp) => {
       this.digiModals = shuffle(resp.map(x => ({name:x.name,img:x.img})));
       this.digiCheckName = resp.map(x => x.name.toLowerCase());
-    })
-  }
+      }
+  })
+}
 
 }
