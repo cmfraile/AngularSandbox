5a03f0393c0108b6a7f058f12cb0905e569ffb5b
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-01-20 18:20:11 +0100

    hasta mañana

--- src/app/components/fifth/fifth.component.ts
+++ src/app/components/fifth/fifth.component.ts
@@ -4,7 +4,6 @@ import { FormAsyncValidatorsService } from 'src/app/services/form-async-validato
 import { AsyncValidatorFn } from '@angular/forms';
 import { Observable ,delay, tap ,map , of } from 'rxjs';
 import { ValidationErrors } from '@angular/forms';
-
 @Component({
   selector: 'app-fifth',
   templateUrl: './fifth.component.html',
@@ -12,13 +11,10 @@ import { ValidationErrors } from '@angular/forms';
 })
 export class FifthComponent {
 
-  validatorMiddleware = ({value}:any):Observable<ValidationErrors | null> => {
+  validatorMiddleware = (form:any):Observable<ValidationErrors | null> => {
     //Haz por usar el of, ya que el Observable esta deprecated para una mayoria de usos.
-    return of<any>(value).pipe(
-      delay(1000),
-      tap(() => { console.log('entrando en el validator',value) }),
-      map(resp => null)
-    )
+    console.log(form)
+    return of<any>(null).pipe(delay(500))
   }
 
   validatorPlaceholder = [Validators.required,Validators.minLength(6)];
